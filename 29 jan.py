7f011c48bdbb4f0755c751717f4563938aae9782
kalyanikaradi <154000642+kalyanikaradi@users.noreply.github.com>
2024-01-29 20:57:33 +0530

    Add files via upload

---
+++ 29 jan.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#TYPES OF ERRORS
+
+
+# In[ ]:
+
+
+Syntax Errors:
+
+Definition: These errors occur when the rules of a programming language are not followed.
+Example: Missing semicolons, mismatched parentheses, or incorrect variable names.
+
+
+# In[ ]:
+
+
+Runtime Errors:
+
+Definition: These errors occur during the execution of a program and can cause it to terminate abruptly.
+Example: Division by zero, accessing an array index out of bounds, or trying to open a non-existent file.
+
+
+# In[ ]:
+
+
+Logical Errors:
+
+Definition: These errors do not cause the program to crash, but they result in incorrect or unexpected behavior.
+Example: Flawed algorithm or incorrect conditional statements.
+
+
+# In[ ]:
+
+
+Semantic Errors:
+
+Definition: These errors involve incorrect understanding of the meaning of code, leading to unintended results.
+Example: Incorrect usage of variables, functions, or operators.
+
+
+# In[ ]:
+
+
+Compilation Errors:
+
+Definition: These errors occur during the compilation phase of a program, preventing it from being translated into machine code.
+Example: Undefined variables, missing libraries, or incompatible data type
+
+
+# In[ ]:
+
+
+Arithmetic Errors:
+
+Definition: These errors involve mathematical operations and can result in incorrect calculations.
+Example: Overflow (resulting in a value too large for the data type) or underflow (resulting in a value too small).
+
+
+# In[ ]:
+
+
+Network Errors:
+
+Definition: These errors occur in computer networks and can affect communication between devices.
+
+
+# In[ ]:
+
+
+Human Errors:
+
+Definition: Mistakes made by humans during the design, coding, or data entry phases.
+Example: Typographical errors, incorrect data input, or misinterpretation of requirements.
+
+
+# In[ ]:
+
+
+Systematic Errors:
+
+Definition: Persistent errors that consistently affect results in the same way.
+Example: Instrument calibration errors, biased measurements, or flawed algorithms.
+
+
+# In[ ]:
+
+
+Random Errors:
+
+Definition: Unpredictable errors that occur randomly and can affect the accuracy of measurements.
+Example: Environmental noise, interference, or unpredictable system behavior.
+
